ef97e60965080b87b8667c4d0ffbf9844ab45163
zonekrish <107716360+zonekrish@users.noreply.github.com>
2024-01-13 08:54:48 -0600

    Start using BeautifulSoup

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # Import necessary dependencies
 import tweepy
+import requests
+from bs4 import BeautifulSoup
 from secret import credentials
+
+# Assign URL to scrape
+url = "https://apod.nasa.gov/apod/archivepixFull.html"
 
 # Log into API
 auth = tweepy.OAuth1UserHandler(
@@ -19,7 +24,20 @@ client = tweepy.Client(
 )
 
 def tweet():
+    # Get HTML page
+    response = requests.get(url)
+
+    # Parse page
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Get all elements w/ image
+    elements = soup.find_all("a")
+
+    # Print all elements
+    for i in range(len(elements)):
+        print(elements[i])
+
     # Create tweet
-    client.create_tweet(text="...still love space")
+    # client.create_tweet(text="...still love space")
 
 tweet()
