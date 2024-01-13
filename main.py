69cc3f7a665549d77bf49de7a078241e7e884625
zonekrish <107716360+zonekrish@users.noreply.github.com>
2024-01-12 21:40:24 -0600

    Implement tweepy

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+# Import necessary dependencies
 import tweepy
+from secret import credentials
+
+# Log into API
+auth = tweepy.OAuth1UserHandler(
+    credentials["API_KEY"],
+    credentials["API_KEY_SECRET"],
+    credentials["ACCESS_TOKEN"],
+    credentials["ACCESS_TOKEN_SECRET"]
+)
+api = tweepy.API(auth)
+client = tweepy.Client(
+    credentials["BEARER_TOKEN"],
+    credentials["API_KEY"],
+    credentials["API_KEY_SECRET"],
+    credentials["ACCESS_TOKEN"],
+    credentials["ACCESS_TOKEN_SECRET"]
+)
+
+# Create tweet
+client.create_tweet(text="I love space!")
