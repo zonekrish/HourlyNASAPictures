582deb80dfac5069b4cdd3701113598af59c27a8
zonekrish <107716360+zonekrish@users.noreply.github.com>
2024-01-14 13:43:04 -0600

    Remove scheduling stuff (thanks Lambda)

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from secret import credentials
 def tweet():
     # Remove image
     try:
-        os.remove("temp.jpg")
+        os.remove("/tmp/temp.jpg")
     except:
         pass
 
@@ -54,7 +54,7 @@ def tweet():
     # Download image from link
     imgData = requests.get(link).content
 
-    with open("temp.jpg", "wb") as f:
+    with open("/tmp/temp.jpg", "wb") as f:
         f.write(imgData)
     
     # Set up image metadata
@@ -62,27 +62,7 @@ def tweet():
     print(metadata.strip())
 
     # Upload photo to Twitter
-    img = api.simple_upload("temp.jpg")
+    img = api.simple_upload("/tmp/temp.jpg")
     api.create_media_metadata(img.media_id, alt_text=metadata[7:])
 
     post = client.create_tweet(media_ids=[img.media_id])
-
-# Check if user specified scheduled Tweets
-def checkArgs():
-    for i in range(len(sys.argv)):
-        if (sys.argv[i] == "schedule"):
-            return True
-    
-    return False
-
-if (checkArgs()):
-    import schedule
-    import time
-
-    schedule.every().hour.at(":00").do(tweet)
-
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-else:
-    tweet()
